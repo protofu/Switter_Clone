f7993db3ec322d2e58196622b57b0b5288e2ea11
protofu <sungjae0512@gmail.com>
2024-03-31 18:05:30 +0900

    #nomad clone coding router settings 240331

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Layout from "./components/layout"
+import Home from "./routes/home"
+import Profile from "./routes/profile"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "Profile",
+        element: <Profile />,
+      }
+    ]
+  }
+])
+
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
